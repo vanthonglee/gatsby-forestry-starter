e26a132e599b811fe272c88b5c0d60bb4bb5fe79
thonglevan131 <thonglevan131@gmail.com>
2020-10-05 00:53:12 +0700

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    {
       resolve: "gatsby-plugin-page-creator",
       options: {
         path: `${__dirname}/posts`,
@@ -35,6 +42,31 @@ module.exports = {
         defaultLayouts: {
           // posts: require.resolve("./template/posts-layout.js"),
           default: require.resolve("./template/blog.js"),
+        },
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/content/tin-tuc`,
+        createPath: (basePath, filePath) => {
+          // Creates a custom URL based on filename
+          return `/tin-tuc/${slugify(basename(filePath))}/`;
         },
       },
     },
